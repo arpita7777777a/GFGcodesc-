e5a272e9bd4619465d02b2f8fb496be69ca521ff
arpita7777777a <78135660+arpita7777777a@users.noreply.github.com>
2023-03-30 19:38:52 +0530

    Update move_all_zeros_to_the_end_of_array.cpp

--- move_all_zeros_to_the_end_of_array.cpp
+++ move_all_zeros_to_the_end_of_array.cpp
@@ -2,13 +2,23 @@ class Solution{
 public:
 	void pushZerosToEnd(int arr[], int n) {
 	    // code here
-	    int j=0;
-	    for(int i=0;i<n;i++){
-	        if(arr[i] !=0){
-	            swap(arr[j],arr[i]);
-	            j++;
-	            
-	        }
-	    }
+		 int i = 0, j = 0;
+
+    // Move all non-zero elements to the front of the array
+    while (i < n) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+        i++;
+    }
+
+    // Fill the remaining elements with zeros
+    while (j < n) {
+        arr[j] = 0;
+        j++;
+    }
+}
+
 	}
 };
